6870f93049f0c69038ab54931449c075ae1c6852
Mohamed Babela <mbabela@e1r13p2.1337.ma>
2024-02-28 15:16:57 +0100

    init parser

---
+++ includes/tokenizer.py
@@ -0,0 +1,47 @@
+from enum import Enum
+
+# Importing macros from parser_macro.py
+from parser_macro import *
+
+
+class TokenType(Enum):
+    EWHITESPACE = 0
+    ENAME = 1
+    ERATIONAL = 2
+    EIMAGINARY = 3
+    EDELIMITER = 4
+    ESYMBOL = 5
+    EEXPRESSION = 6
+    EEMPTY = 7
+
+
+class Tokenizer:
+    def __init__(self, raw):
+        self._raw = raw
+        self._current_pos = 0
+
+    def __del__(self):
+        pass
+
+    # Main function
+    def scan_token(self, holder):
+        pass
+
+    # Exception classes
+    class BadFormat(Exception):
+        def __str__(self):
+            return "Input has bad format"
+
+    class EmptyInput(Exception):
+        def __str__(self):
+            return "Input is empty"
+
+    # Parsing utils
+    def _find(self, charset):
+        pass
+
+    def _find_not_of(self, charset, pos=None):
+        pass
+
+    def _update_token(self, next_pos):
+        pass
